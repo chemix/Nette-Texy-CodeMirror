1bd5efdf823dcdd5973ea9b3013ab120cf4edf1c
chemiX <iam@chemix.cz>
2014-11-17 00:58:13 +0100

    add form

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,6 +5,9 @@ namespace App\Presenters;
 use Nette,
 	App\Model;
 
+use Nette\Application\BadRequestException;
+use Nette\Application\ForbiddenRequestException;
+use Nette\Application\UI\Form;
 
 /**
  * Homepage presenter.
@@ -14,6 +17,75 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
+	}
+
+	/**
+	 * TODO: use Nette way
+	 * Prototype of save image
+	 */
+	public function actionSaveimage()
+	{
+		$response = array();
+		/*
+		if (!$this->user->isAllowed('Article', 'edit')) {
+			$response['error'] = 'Error access denied!';
+			$this->sendJson($response);
+		}
+		*/
+
+		$uploadFolder = __DIR__ . '/../../www/data/articles/';
+		$onlinePath = '/data/articles/';
+
+		if (isset($_FILES['file'])) {
+			$file = $_FILES['file'];
+			$filename = uniqid() . '.' . (pathinfo($file['name'], PATHINFO_EXTENSION) ? : 'png');
+
+			move_uploaded_file($file['tmp_name'], $uploadFolder . $filename);
+
+			$response['filename'] = $onlinePath . $filename;
+		} else {
+			$response['error'] = 'Error while uploading file';
+		}
+
+		$this->sendJson($response);
+	}
+
+
+	/**
+	 * Article form factory.
+	 *
+	 * @return \Nette\Application\UI\Form
+	 */
+	protected function createComponentArticleForm()
+	{
+		$form = new Form;
+
+		$form->addTextArea('content', 'Content:')
+			->setRequired('Please enter content.');
+
+		$form->addSubmit('send', 'Add article');
+
+		$form->onSuccess[] = $this->articleFormSucceeded;
+
+		return $form;
+	}
+
+
+	/**
+	 * @param Form $form
+	 * @throws ForbiddenRequestException
+	 */
+	public function articleFormSucceeded(Form $form, $values)
+	{
+		/*
+		if (!$this->user->isAllowed('Article', 'edit')) {
+			throw new ForbiddenRequestException();
+		}
+		*/
+
+		dump($values);
+		exit();
+
 	}
 
 }
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,48 @@
 $(function(){
 
+    var codeEditor = $('#code');
+    if (codeEditor[0]) {
+        console.log('init CodeMirror');
+
+        var map = {
+            "Cmd-B": function (cm) {
+                var selection = cm.doc.getSelection();
+                if (selection) {
+                    cm.doc.replaceSelection('**' + selection + '**');
+                } else {
+                    var cur = cm.doc.getCursor();
+                    cm.doc.replaceRange('****', cur);
+                    cm.doc.setCursor(cur.line, cur.ch + 2);
+                }
+            },
+            "Cmd-I": function (cm) {
+                var selection = cm.doc.getSelection();
+                if (selection) {
+                    cm.doc.replaceSelection('*' + selection + '*');
+                } else {
+                    var cur = cm.doc.getCursor();
+                    cm.doc.replaceRange('**', cur);
+                    cm.doc.setCursor(cur.line, cur.ch + 1);
+                }
+            },
+            "Cmd-S": function (cm) {
+                // todo... ajax save
+            }
+        };
+
+        var editor = CodeMirror.fromTextArea(codeEditor[0], {
+            theme: "monokai"
+            //, viewportMargin: "Infinity"
+        });
+
+        var options = {
+            uploadUrl: basePath + '/Homepage/save-image',
+            urlText: "\n[*{filename}*]\n"
+        }
+
+        inlineAttachment.editors.codemirror4.attach(editor, options);
+
+        editor.addKeyMap(map);
+    }
+
 });
